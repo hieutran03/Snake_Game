0bde945de6280d3d1063948daa11488476c185eb
hieutran03 <96785351+hieutran03@users.noreply.github.com>
2022-07-27 09:44:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ typedef coordinate coor;
 
 int main(){
 	system("color 70");
+	int point = 0;
 	char graph[X][Y];
 	initMap(graph);
 	snake sna;
@@ -18,20 +19,23 @@ int main(){
 	createApple(graph);
 	showMap(graph);
 	cout << endl;
+	cout << "YOUR POINT: " << point << endl;
 	char option;
 	int life = 3;
 	do{
 		option = getch();
 		system("cls");
 		
-		if(!snakeMove(graph,sna,option))
+		if(!snakeMove(graph,sna,option, point))
 		{
 			showMap(graph);
+			cout << "YOUR POINT: " << point << endl;
 			life--;
 			cout << "CANT'MOVE" << endl;
-			cout << "YOUR LIFE: " << life;
+			cout << "YOUR LIFE: "  << life;
 		}else{
 			showMap(graph);
+			cout << "YOUR POINT: " << point << endl;
 		}
 		cout << endl;
 	}while(option != 'x'&& life > 0);
