07c90905755421b5380028c188fb0d1bf0c11aa8
Hieu Tran <96785351+hieutran03@users.noreply.github.com>
2022-07-28 10:35:08 +0700

    Update coordinate.h

--- coordinate.h
+++ coordinate.h
@@ -6,22 +6,29 @@
 
 struct coordinate{
 	int x, y;
-	void set(int x, int y){
+	void set_coor(int x, int y){
 		this->x = x;
 		this->y = y;
 	}
-
-	void operator = (coordinate c){
-		this->x = c.x;
-		this->y = c.y;
+	coordinate get_coor(){
+		return *this;
 	}
+	
 	void set_char(char graph[][Y], char _char){
 		graph[x][y] = _char;
 	}
 	char get_char(char graph[][Y]){
 		return graph[x][y];
 	}
-
+	void operator = (coordinate c){
+		this->x = c.x;
+		this->y = c.y;
+	}
+	bool operator == (coordinate c){
+		if(this->x == c.x && this->y == c.y)
+			return 1;
+		return 0;	
+	}
 }; 
 typedef struct coordinate coor;
 #endif
